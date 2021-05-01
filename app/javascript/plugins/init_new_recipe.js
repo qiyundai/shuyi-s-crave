ebd84b56b8c2d431e57710c469652b0932b1fae0
Qiyun Dai <pianoquin@gmail.com>
2021-05-01 14:16:46 -0500

    found the syntax to pass array into params

--- app/javascript/plugins/init_new_recipe.js
+++ app/javascript/plugins/init_new_recipe.js
@@ -1,10 +1,9 @@
 const addIngredient = () => {
   const $addIngredient = document.getElementById('ingredient-add');
   if ($addIngredient) {
-    const ingredientInputHTML = '<div class="line-ingredient"><input class="ingredient" placeholder="Ingredient" type="text" name="ingredient" id="ingredient"> <input class="amount" placeholder="Amount" type="text" name="amount" id="amount"> <input class="unit" placeholder="Unit" type="text" name="unit" id="unit"></div>';
+    const ingredientInputHTML = '<div class="line-ingredient"><input class="ingredient" placeholder="Ingredient" type="text" name="ingredient[]" id="ingredient"> <input class="amount" placeholder="Amount" type="text" name="amount[]" id="amount"> <input class="unit" placeholder="Unit" type="text" name="unit[]" id="unit"></div>';
 
     $addIngredient.addEventListener('click', () => {
-      console.log(EventTarget)
       $addIngredient.insertAdjacentHTML('beforebegin', ingredientInputHTML)
     })
   }
