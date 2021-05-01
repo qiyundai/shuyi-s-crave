6bd1a3eb0fbb8f9dd1887f26c02568507b83e1e6
Qiyun Dai <pianoquin@gmail.com>
2021-05-01 13:58:46 -0500

    refactored the JS for new_recipe. Removed old form. Still can't get the params to work correctly.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,22 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { addIngredient } from '../plugins/init_new_recipe'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-const addIngredient = () => {
-  const $addIngredient = document.getElementById('ingredient-add');
-  if ($addIngredient) {
-    const ingredientInputHTML = '<div class="line-ingredient"><input class="ingredient" placeholder="Ingredient" type="text" name="ingredient"> <input class="amount" placeholder="Amount" type="text" name="amount"> <input class="unit" placeholder="Unit" type="text" name="unit" id="unit"></div>';
-
-    $addIngredient.addEventListener('click', () => {
-      console.log(EventTarget)
-      $addIngredient.insertAdjacentHTML('beforebegin', ingredientInputHTML)
-    })
-  }
-}
 
 document.addEventListener("turbolinks:load", () => {
   addIngredient();
---
+++ app/javascript/plugins/init_new_recipe.js
@@ -0,0 +1,13 @@
+const addIngredient = () => {
+  const $addIngredient = document.getElementById('ingredient-add');
+  if ($addIngredient) {
+    const ingredientInputHTML = '<div class="line-ingredient"><input class="ingredient" placeholder="Ingredient" type="text" name="ingredient" id="ingredient"> <input class="amount" placeholder="Amount" type="text" name="amount" id="amount"> <input class="unit" placeholder="Unit" type="text" name="unit" id="unit"></div>';
+
+    $addIngredient.addEventListener('click', () => {
+      console.log(EventTarget)
+      $addIngredient.insertAdjacentHTML('beforebegin', ingredientInputHTML)
+    })
+  }
+}
+
+export { addIngredient };
